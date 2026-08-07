bf7dccc9e2edd47bb7dd4ba7ce13f624699c3f64
Brian-Ckwu <sonny40920710@gmail.com>
2026-08-07 15:29:58 +0800

    script(ckwu): update script for training on H100*2

--- cs336_alignment/run_grpo_cc.sh
+++ cs336_alignment/run_grpo_cc.sh
@@ -3,20 +3,21 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-model_id="Qwen/Qwen3-1.7B"
-lr=3e-5
+model_id="Qwen/Qwen3-8B"
+lr=1e-5
 # _lora-r-16-a-32-dropout-0-bf16
 uv run training_script_grpo_on_cc.py \
   --wandb-project-name "GRPO_CC_TriviaQA" \
-  --wandb-exp-name "${model_id}_lr-${lr}_lora-r-16-a-32-dropout-0-bf16_temp-0.6_ntrain-73600" \
+  --wandb-exp-name "${model_id}_lr-${lr}_lora-r-16-a-32-dropout-0-bf16_h100" \
   --model-id "${model_id}" \
-  --policy-device 2 \
-  --rollout-device 3 \
+  --dataset_path "../data/capability_calibration/triviaqa-train__Qwen3-8B-non-thinking/grpo_dataset.jsonl" \
+  --policy-device 0 \
+  --rollout-device 1 \
   --gpu-memory-utilization 0.9 \
-  --prompt-path "prompts/verbalized_cc.prompt" \
-  --n-train-examples 73600 \
-  --n-val-examples 2048 \
-  --num-rollout-steps 2300 \
+  --prompt-path "prompts/verbalized_cc_5-shots.prompt" \
+  --n-train-examples 12800 \
+  --n-val-examples 1024 \
+  --num-rollout-steps 400 \
   --learning-rate "${lr}" \
   --rollout-batch-size 256 \
   --train-batch-size 256 \
